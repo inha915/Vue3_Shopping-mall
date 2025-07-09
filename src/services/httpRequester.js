ea16d7620a0ab1b08bda5b22cd40025850cf64be
inha915 <dlsgk0915@gmail.com>
2025-07-09 09:28:24 +0900

    갤러리쇼핑몰

---
+++ src/services/httpRequester.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+axios.defaults.baseURL = '/api/v1';
+
+export default axios;
+// accountService.js와 itemService.js의 공통된 내용을 적은 js 파일
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { getItems } from '@/services/itemService';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const res = await getItems();
+  console.log(res.data);
+});
+</script>
+<!-- 이 화면이 마운트될 때 딱 한번 getItems 통신하고 
+ 통신으로 받아온 데이터를 콘솔에 나타내시오 -->
+
+<template>
+  <div class="container">
+    <h1 class="mt-5">HELLO, HOME</h1>
+  </div>
+</template>
+
+<style scoped></style>
